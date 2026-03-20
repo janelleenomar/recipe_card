9e65e960fc922a464bdeacc10cac6ad6f19574d0
Rose Janelle Enomar <janelleenomar@gmail.com>
2026-03-20 22:48:19 +0800

    feat: add main recipe card container

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,22 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xFFDDE6ED),
         body: Center(
+          child: Container(
+            width: 337,
+            height: 600,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5F4EA),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withAlpha(25),
+                  spreadRadius: 10,
+                  blurRadius: 15,
+                  offset: Offset(5, 10),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
